25a9759be20d9b2580156627590670b12257b362
Mihailo Aleksić <mihailo.aleksic@partner.digitalchargingsolutions.com>
2023-06-24 12:41:40 +0200

    Initial project

---
+++ bar-explorer/src/main/kotlin/decemo/com/barexplorer/BarExplorerApplication.kt
@@ -0,0 +1,11 @@
+package decemo.com.barexplorer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BarExplorerApplication
+
+fun main(args: Array<String>) {
+    runApplication<BarExplorerApplication>(*args)
+}
